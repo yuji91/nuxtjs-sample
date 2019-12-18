2356fd7b8af23139b29771e0c6a94c55826a275d
yuji91 <yuji91.1227@gmail.com>
2019-12-19 00:08:52 +0900

    Add dynamic routing

--- docker/nuxt.js/src/app/pages/users/_id.vue
+++ docker/nuxt.js/src/app/pages/users/_id.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <p>
-      /users/_id.vue
+      User ID: {{ userId }}
     </p>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+          userId: this.$route.params.id
+        }
+     }
+   }
+</script>
